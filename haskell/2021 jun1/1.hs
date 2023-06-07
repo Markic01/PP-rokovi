ba9705a049ffa8b169dbbd806e59291a78b435d6
Markic01 <01marko.radosavljevic@gmail.com>
2023-06-07 23:31:04 +0200

    haskell 2021 jun1

---
+++ haskell/2021 jun1/1.hs
@@ -0,0 +1,20 @@
+ubaci x list = x:list
+
+dodaj :: [(Int, Char)] -> (Int, Char) -> [(Int, Char)]
+dodaj [] karta = [karta]
+dodaj (x:xs) karta
+        | fst x == fst karta && snd x == snd karta = (x:xs)
+        | fst x < fst karta = x:(dodaj xs karta)
+        | fst x >= fst karta = ubaci karta (x:xs)
+
+ukloni :: [(Int, Char)] -> (Int, Char) -> [(Int, Char)]
+ukloni [] _ = []
+ukloni (x:xs) karta
+        | x == karta = xs
+        | otherwise = x : (ukloni xs karta)
+
+uporedi [] [] = EQ
+uporedi (x:xs) (y:ys)
+        | (fst x) < (fst y) = LT
+        | (fst x) > (fst y) = GT
+        | otherwise = uporedi xs ys
